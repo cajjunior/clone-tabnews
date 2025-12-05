0a1e00f5795aff0cd499a8d65c902bef9fa31f16
Carlos A. Julio Jr. <junior@carlosjulio.com.br>
2025-12-05 18:33:29 +0000

    adiciona a página inicial

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+function Home() {
+  return (
+    <h1
+      style={{
+        color: "darkblue",
+        fontSize: "30px",
+        fontFamily: "Athenian",
+        textAlign: "neter",
+        padding: "20px"
+      }}
+    >
+      Olá, Tais! Se você acha que eu posso ser um bom programador faça um jóia 👍 !!
+    </h1>
+  );
+}
+
+export default Home;
